ffd058ce9147d3564e979f5170896dc93cc2c84c
OliaPopova <olia.popova2121@gmail.com>
2023-10-03 18:01:44 +0300

    03.10.23 выполнены задания домашние

--- Seminars and HW/2_26.09.23/Actor.java
+++ Seminars and HW/2_26.09.23/Actor.java
@@ -1,13 +1,44 @@
-public abstract class Actor implements ActorBehaviour {
-    
-    protected final String name;
-    protected boolean isTakeOrder;
+public abstract class Actor implements ActorBehaviour{
+
+    protected String name;
+    protected String ticketNumber;
+
     protected boolean isMakeOrder;
 
-    public Actor (String name) {this.name = name;}
+    protected boolean isTakeOrder;
+    public Actor(String name) {
+        this.name = name;
+    }
 
-    public abstract String getName();
+    abstract String getName();
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getTicketNumber() {
+        return ticketNumber;
+    }
 
+    public void setTicketNumber(String ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    @Override
+    public boolean isMakeOrder() {
+        return isMakeOrder;
+    }
+
+    public void setMakeOrder(boolean makeOrder) {
+        isMakeOrder = makeOrder;
+    }
+
+    @Override
+    public boolean isTakeOrder() {
+        return isTakeOrder;
+    }
+
+    public void setTakeOrder(boolean takeOrder) {
+        isTakeOrder = takeOrder;
+    }
 }
--- Seminars and HW/2_26.09.23/Market.java
+++ Seminars and HW/2_26.09.23/Market.java
@@ -1,3 +1,86 @@
-public class Market {
-    
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
+public class Market implements MarketBehaviour, QueueBehaviour {
+    private List<Actor> queue;
+
+
+    public Market(List<Actor> queue) {
+        this.queue = new ArrayList<>(queue);
+    }
+
+    @Override
+    public void takeInQueue(Actor actor) {
+        queue.add(actor);
+    }
+
+    @Override
+    public void takeOrders() {
+        for (Actor actor : queue) {
+            actor.setMakeOrder(true);
+            actor.setTicketNumber(QueueTicketGenerator.generateTicket());
+            System.out.println(actor.getName() + " сделал свой заказ. Номер заказа: " + actor.getTicketNumber() + ".");
+        }
+        System.out.println();
+
+    }
+
+    @Override
+    public void giveOrders() {
+        for (Actor actor : queue) {
+            actor.setTakeOrder(true);
+            System.out.println(actor.getName() + " забрал свой заказ " + actor.getTicketNumber()+ ".");
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void releaseFromQueue() {
+        List<Actor> realeasedActors = new ArrayList<>();
+        for (Actor actor : queue) {
+            if (actor.isTakeOrder) {
+                realeasedActors.add(actor);
+//                System.out.println(actor.getName() + " вышел из очереди.");
+            }
+        }
+        releaseFromMarket(realeasedActors);
+    }
+
+    @Override
+    public void acceptToMarket(Actor actor) {
+        takeInQueue(actor);
+    }
+
+    @Override
+    public void releaseFromMarket(List<Actor> actors) {
+        for (Actor actor : actors) {
+            System.out.println(actor.getName() + " вышел из магазина.");
+            queue.remove(actor);
+        }
+    }
+
+    @Override
+    public void update() throws InterruptedException {
+        Runnable task = ()-> {
+                takeOrders();
+                giveOrders();
+                releaseFromQueue();
+            System.out.println("\n--- Ожидание новых посетителей!---\n");
+        };
+        Thread thread = new Thread(task);
+        Random random = new Random();
+        thread.start();
+        Thread.sleep(random.nextInt(3000,7000));
+    }
+
+    public List<Actor> getQueue() {
+        return queue;
+    }
+
+    public void setQueue(List<Actor> queue) {
+        this.queue = queue;
+    }
+
 }
